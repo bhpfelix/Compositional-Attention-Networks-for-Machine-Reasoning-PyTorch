79fc2dc25db26fe73796a13aca690f6a7ea7ebd1
Haoping Bai <bhpfelix@gmail.com>
2018-03-24 17:39:18 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,19 +3,22 @@ USE_CUDA = torch.cuda.is_available()
 
 GLOVE_PATH = 'glove.json'
 ANSWER_PATH = 'answers.json'
-CORPUS = '42B' # '840B'
-D_POS_VEC = 16 # 128
+# CORPUS = '42B' # '840B'
+# D_POS_VEC = 16 # 128
 TRAIN_IM_FEATS = 'train.hdf5'
-TRAIN_QUESTION = '/disk2/CLEVR_v1.0/questions/CLEVR_train_questions.json'
+TRAIN_QUESTION = 'train_questions.hdf5' # '/disk2/CLEVR_v1.0/questions/CLEVR_train_questions.json'
 VAL_IM_FEATS = 'val.hdf5'
-VAL_QUESTION = '/disk2/CLEVR_v1.0/questions/CLEVR_val_questions.json'
+VAL_QUESTION = 'val_questions.hdf5' # '/disk2/CLEVR_v1.0/questions/CLEVR_val_questions.json'
 
-BATCH_SIZE = 64 #
+VOCAB_SIZE = 80 # not including <NULL>
+BATCH_SIZE = 64 # 
 NET_PARAM = {
     'd':512,
-    'input_in_channels':1024 + 2 * D_POS_VEC,
+    'vocab_size':80 + 1, # +1 for <NULL> token padding
+    'embedding_dim':300,
+    'input_in_channels':1024, # + 2 * D_POS_VEC,
     'input_hidden_channels':None,
-    'p':1,
+    'p':12,
     'write_self_attn':True, # False, #
     'write_mem_gate':True,
     'num_answers':28,
@@ -23,8 +26,8 @@ NET_PARAM = {
 }
 
 OPT_PARAM = {
-    'lr':0.0001,
-    'betas':(0.9, 0.999),
-    'eps':1e-08,
+    'lr':0.0001, 
+    'betas':(0.9, 0.999), 
+    'eps':1e-08, 
     'weight_decay':0 # 0.999
 }
